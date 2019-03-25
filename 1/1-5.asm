0072f1e2a32b23fa3a7fd40d7a6e57ce8106f17e
winderica <winderica@gmail.com>
2019-03-25 20:55:09 +0800

    Update: finish 1

--- 1/1-5.asm
+++ 1/1-5.asm
@@ -7,8 +7,12 @@ data segment use16
     item_number     equ 30
     username_hint   db  'Input your username:', 13, 10, '$'
     password_hint   db  'Input your password:', 13, 10, '$'
+    succeeded_hint  db  'Login succeeded', 13, 10, '$'
     failed_hint     db  'Login failed, please input again', 13, 10, '$'
+    not_found_hint  db  'Item not found, please input again', 13, 10, '$'
     item_hint       db  'Input item name:', 13, 10, '$'
+    level_hint      db  'Suggestion level is:', 13, 10, '$'
+    calc_fail_hint  db  'Error: Divide by zero!', 13, 10, '$'
     boss_username   db  'zhangchengru', 0
     boss_password   db  'test', 0, 0
     input_username  db  20
@@ -22,7 +26,7 @@ data segment use16
                     dw  35, 56, 70, 25, ?
     item_2          db  'book', 6 dup(0), 9
                     dw  12, 30, 25, 5, ?
-    item_temp       db  item_number - 2 dup('item_temp', 8, 15, 0, 20, 0, 30, 0, 2, 0, ?, ?)
+    item_temp       db  item_number - 2 dup('item_temp', 0, 8, 15, 0, 20, 0, 30, 0, 2, 0, ?, ?)
     auth            db  0
 data ends
 
@@ -83,7 +87,7 @@ check_username:
     jne     login_failed
     inc     si
     inc     bx
-    cmp     byte ptr[si], 0
+    cmp     byte ptr[bx], 13
     je      verify_password
     jmp     check_username
 
@@ -96,11 +100,14 @@ check_password:
     jne     login_failed
     inc     si
     inc     bx
-    cmp     byte ptr[si], 0
+    cmp     byte ptr[bx], 13
     je      login_succeed
     jmp     check_password
 
 login_succeed:
+    mov     dx, offset succeeded_hint
+    mov     ah, 09h
+    int     21h
     mov     auth, 1
     jmp     query_item
 
@@ -127,7 +134,7 @@ check_item:
     jne     next_item
     inc     si
     inc     bx
-    cmp     byte ptr[si], 0
+    cmp     byte ptr[bx], 13
     je      item_found
     jmp     check_item
 next_item:
@@ -136,12 +143,17 @@ next_item:
     mov     bx, offset input_item + 2
     add     cx, 1
     cmp     cx, item_number
-    je      query_item
+    je      item_not_found
     jmp     loop_item
 item_found:
     cmp     auth, 1
     je      show_name
-    jmp     calc_suggestion
+    jmp     calc_suggestion_level
+item_not_found:
+    mov     dx, offset not_found_hint
+    mov     ah, 09h
+    int     21h
+    jmp     query_item
 show_name:
     mov     byte ptr[bx], '$'
     mov     dx, offset input_item + 2
@@ -150,12 +162,17 @@ show_name:
     call    print_newline
     mov     auth, 0
     jmp     login
-calc_suggestion:
+calc_suggestion_level:
 get_discount:
     inc     si
     cmp     byte ptr[si], 0
     jne     calc_price
     jmp     get_discount
+calc_failed:
+    mov     dx, offset calc_fail_hint
+    mov     ah, 09h
+    int     21h
+    jmp     query_item
 calc_price:
     movzx   ax, byte ptr[si]
     inc     si
@@ -166,20 +183,34 @@ calc_price:
     idiv    bx
     mov     bx, ax
     mov     ax, word ptr[si]
-    imul    ax, 128
+    imul    ax, 1280
     cwd
     mov     dx, 0
+    cmp     bx, 0
+    je      calc_failed
     idiv    bx
     mov     cx, ax
     mov     bx, word ptr[si+4]
     mov     ax, word ptr[si+6]
-    imul    ax, 128
+    imul    ax, 1280
     cwd
     mov     dx, 0
+    cmp     bx, 0
+    je      calc_failed
     idiv    bx
     add     cx, ax
+    mov     ax, cx
+    cwd
+    mov     dx, 0
+    mov     bx, 10
+    idiv    bx
+    mov     cx, ax
+    mov     word ptr[si+8], cx
+    mov     dx, offset level_hint
+    mov     ah, 09h
+    int     21h
     cmp     cx, 100
-    jg       show_a
+    jg      show_a
     cmp     cx, 50
     jg      show_b
     cmp     cx, 10
